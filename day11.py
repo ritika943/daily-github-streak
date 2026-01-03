34f750fadf9030286e90d30c7d50501227a09cf4
Ritika_Raj <ritikakashyap2409@gmail.com>
2026-01-03 22:39:28 +0530

    Day 11: simple python update

---
+++ day11.py
@@ -0,0 +1,9 @@
+# Day 11 GitHub Streak
+# Showing up even when motivation is low
+
+def day_11():
+    print("Day 11 completed ✅")
+    print("GitHub streak maintained")
+    print("Consistency matters")
+
+day_11()
